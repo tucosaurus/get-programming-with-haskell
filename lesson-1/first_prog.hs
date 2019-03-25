4a22c3a525eb843402f4e15307abaaed490a0877
Akash Mishra <tucosaurus@gmail.com>
2019-03-26 00:58:41 +0530

    ch1 - modified first program in haskell

--- lesson-1/first_prog.hs
+++ lesson-1/first_prog.hs
@@ -1,3 +1,13 @@
+toPart recipient = "Dear " ++ recipient ++ ",\n"
+
+bodyPart title = "Thanks for buying " ++ title ++ "\n"
+
+fromPart author = "Thanks " ++ author
+
+createEmail recipient title author = toPart recipient ++
+                                      bodyPart title ++
+                                      fromPart author
+
 main = do
     print "Who is the email for?"
     recipient <- getLine
@@ -5,6 +15,7 @@ main = do
     title <- getLine
     print "Who is the Author?"
     author <- getLine
-    print ("Dear " ++ recipient ++ ",\n" ++
-      "Thanks for buying " ++ title  ++ "\nthanks,\n" ++
-      author )
+    -- The function returned a string (a value)
+    -- instead of printing the string
+    -- side effect free function
+    print (createEmail recipient title author)
